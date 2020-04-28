fc889fc8c0a1be5dbeef38db1a85dcac3e759f03
udosinachi <udosipat@gmail.com>
2020-04-28 13:51:19 +0100

    All values without styling

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,7 +13,16 @@ class Body extends React.Component{
                 </div>
                 <div>
                     <form>
-                        <input />
+                        <p>Username</p>
+                        <input type='text' placeholder='Pick a username' />
+                        <p>Email</p>
+                        <input type='email' placeholder='Your email address' />
+                        <p>Password</p>
+                        <input placeholder='Password' />
+                        <p>Use at least one letter, one number and seven character</p>
+                        <button>Sign up for udohub</button>
+                        <p>By clicking 'Sign up for udohub', you agree to our terms and services and privacy policy.
+                             We will occassionally send your account emails.</p>
                     </form>
                 </div>
             </div>
